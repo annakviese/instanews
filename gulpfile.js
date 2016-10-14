f729c0e27415e6d814f74ee30680ff87d8d9b1cd
Anna Kviese <anna.kviese@gmail.com>
2016-10-14 13:57:34 -0700

    added Linter and plumber to check for errors

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,21 @@ var gulp = require('gulp'),
     eslint = require('gulp-eslint'),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
-    cssnano = require('gulp-cssnano');
+    cssnano = require('gulp-cssnano'),
+    notify = require('gulp-notify');
+
+var plumberErrorHandler={
+    errorHandler: notify.onError({
+      title: 'Gulp',
+      message: 'Error: <%= error.message %>' //will see a notification if not able to compile sass file
+    })
+}; 
 
 
 //uglify 
 
 gulp.task('js', function(){ 
     gulp.src('./js/*.js')//these are the files that gulp will work with  (all of my .js files in js folder)
-      // .pipe(plumber())
       .pipe(uglify())//uglify minifies the js files  .pipe allows to run miltiple methods 
       .pipe(rename({ extname: '.min.js' }))//changes the name and replaces .js to .min.js 
       .pipe(gulp.dest('./build/js')) //putting all the files in the new folder build
@@ -25,6 +32,7 @@ gulp.task('js', function(){
 
 gulp.task('sass', function() {
    gulp.src('./sass/style.scss')
+      .pipe(plumber(plumberErrorHandler))
       .pipe(sass())
       .pipe(autoprefixer({
          browsers: ['last 2 versions']
@@ -54,16 +62,16 @@ gulp.task('browserSync', function () {
 /**eslint*/
 
  
-// gulp.task('eslint', function () {
-//   return gulp.src('./js/*js')
-//     .pipe(eslint())
-//     .pipe(eslint.format())
-//     .pipe(eslint.failAfterError());
-// });
+gulp.task('lint', function () {
+  return gulp.src('./js/*js')
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
 
 
 gulp.task('watch', function(){
-    gulp.watch('js/*.js', ['js']); //, 'eslint'
+    gulp.watch('js/*.js', ['js','lint']); //
     gulp.watch('sass/*.scss',['sass']);
 });
 
--- js/javascript.js
+++ js/javascript.js
@@ -1,34 +1,37 @@
-$(document).ready(function()){
+$(function(){
 
+//when selection changes show me article
 
+	
+	var articleImg ='';
+$
+('.select').on('change',function(event){
+	$topStories = $('.topStories');
+	$topStories.empty();
+	var article='';
+	var selectSection = $('.select').val();
 
+	$.ajax({
+		method: 'GET',
+		url: 'http://api.nytimes.com/svc/topstories/v2/' +selectSection+ '.json?api-key=1fadf3cc3b294071815fd4c0acbdc944',
+		})  
 
+	.done(function(data) {
+		var n = data.results;
+		
+		$.each (n, function(key, value){
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+			if(value.multimedia.length > 0) {
+		var articleImg= value.multimedia[4].url;
+		
+		article += '<li>';
+		article += '<img src="'+articleImg+'"/>';
+		article += '</li>';
+		}
+		});
+		$topStories.append(article);
+    })
+		console.log('hope?');
+	});
+//
 }); //closing tag for document ready
